2819351a2dd600514e4e147fbb3130da0bf5c5fb
technqvi <38780060+technqvi@users.noreply.github.com>
2022-06-04 21:57:51 +0700

    in progress

--- scripts/invoke.py
+++ scripts/invoke.py
@@ -12,7 +12,45 @@ def store_favorite_number():
    tx.wait(1)
    print("store value successfully")
   
+def add_lucky_number_of_person(key_name:str,lucky_numer:int):
+   xcontract=SimpleStorage[-1]
+   tx=xcontract.addPerson(key_name,lucky_numer,{"from":xdeploy.get_account()})
+   tx.wait(1)
+   print("add person and his number successfully")
+
+def list_favoriteNumber_of_people():
+   xcontract=SimpleStorage[-1]
+   list_people=xcontract.people()
+   for i in range(len(list_people)):
+     print(f"{list_people(i)(0)} of {list_people(i)[1]}")
+     #print(xcontract.people(0)[0])
+
+   
+def get_favoriteNumber_by_name(key_name):
+   xcontract=SimpleStorage[-1]
+   my_number=xcontract.nameToFavoriteNumber(key_name)
+   print(my_number)
+
 
 def main():
-    store_favorite_number()
-    read_favorite_number()
+ xcontract=SimpleStorage[-1]
+ print(xcontract.people(0))
+
+   #  store_favorite_number()
+   #  read_favorite_number()
+
+  # trong-pongthorn ,noie-phiranun
+  # add_lucky_number_of_person("noie-phiranun",50)
+  # get_favoriteNumber_by_name("noie-phiranun")
+  #list_favoriteNumber_of_people
+# list_favoriteNumber_of_people()
+# xcontract=SimpleStorage[-1]
+ #print(xcontract.people(0))
+ #print(xcontract.people(1))
+#   print(xcontract.people(0)[0])
+#   print(xcontract.people(0)[1])
+
+   # xcontract=SimpleStorage[-1]
+   # list_people=xcontract.people()
+   # for i in range(len(list_people)):
+   #   print(f"{list_people(i)(0)} of {list_people(i)[1]}")
